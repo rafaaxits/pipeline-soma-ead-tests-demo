05883687e21612dd1ef30a82dd8cf9f4727c8108
rafaaxits <rafaaxits@gmail.com>
2025-11-20 00:04:07 -0300

    breaking tests

--- test_soma.py
+++ test_soma.py
@@ -5,7 +5,7 @@ def test_soma_positiva():
     with allure.step("Somando dois números positivos"):
         resultado = soma(2,3)
     allure.attach(str(resultado), name="resultado", attachment_type=allure.attachment_type.TEXT)
-    assert resultado == 5
+    assert resultado == 6
 
 def test_soma_negativa():
     assert soma(-1,-4) == -5
